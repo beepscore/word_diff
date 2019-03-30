2f53d8e7727e17430e22c631964c4c6b166995e1
beepscore <support@beepscore.com>
2019-03-30 09:10:29 -0700

    write dwdiff output to file

--- differ.py
+++ differ.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import subprocess
+from subprocess import PIPE, run
 
 
 def text_from_file(filename):
@@ -12,15 +12,29 @@ def text_from_file(filename):
         return text
 
 
-def write_diffs(filename_a, filename_b):
-    args = ['dwdiff', filename_a, filename_b]
-    subprocess.run(args)
+def text_to_file(text, filename):
+    """
+    write file
+    """
+    with open(filename, 'w') as output_file:
+        output_file.write(text)
+        return text
+
+
+def write_diffs(in_filename_a, in_filename_b, out_filename):
+    # https://stackoverflow.com/questions/1996518/retrieving-the-output-of-subprocess-call
+    command = ['dwdiff', in_filename_a, in_filename_b]
+    completed_process = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    text_to_file(completed_process.stdout, out_filename)
 
 
 if __name__ == '__main__':
 
-    filename_a = './data/input/a.txt'
-    filename_b = './data/input/b.txt'
-    write_diffs(filename_a, filename_b)
+    in_a = './data/input/a.txt'
+    in_b = './data/input/b.txt'
+    out_ab = './data/output/ab.txt'
+    out_ba = './data/output/ba.txt'
+    write_diffs(in_a, in_b, out_ab)
+    write_diffs(in_b, in_a, out_ba)
 
 
